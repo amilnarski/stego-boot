645a87c82de8811e61f92f7e24b5826da4268ba3
Aaron Miller <aaron@aaronmiller.in>
2014-12-21 12:54:06 -0500

    Performance work.

--- src/main/java/stego/StegoTool.java
+++ src/main/java/stego/StegoTool.java
@@ -1,31 +1,11 @@
 package stego;
 
 import org.springframework.stereotype.Component;
-import stego.imaging.Pixel;
-
-import java.awt.image.BufferedImage;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
 @Component
 public class StegoTool {
-    public BufferedImage embed(BufferedImage cover, BufferedImage message){
-        checkArgument(cover.getHeight()*cover.getWidth()>= (message.getWidth()*message.getHeight())+32, "Cover is not large enough!");
-        checkArgument(message.getHeight() <= 65536 && message.getWidth() <= 65536 , "Message may only have a maximum dimension of 65,536 pixels!");
-        //build bits to embed
-        StringBuilder toEmbed = new StringBuilder();
-        String widthString = Integer.toBinaryString(message.getWidth());
-
-//        if(widthString.)
-
-
-        for(int x = 0; x < message.getHeight(); x++){
-            for (int y = 0; y < message.getWidth(); y++){
-
-            }
-        }
-        return null;
-    }
 
     public static int getLSB(int i){
         checkArgument(i > -1);
@@ -43,5 +23,9 @@ public class StegoTool {
     public static int getMSB(int i, int position){
         checkArgument(i > -1);
         return (i << 32-position) >>> 31;
+    }
+
+    public static int replaceLSB(int n, int lsb){
+        return (n & ~1) | lsb;
     }
 }
--- src/main/java/stego/main/MainController.java
+++ src/main/java/stego/main/MainController.java
@@ -2,7 +2,6 @@ package stego.main;
 
 import org.apache.sanselan.ImageReadException;
 import org.apache.sanselan.Sanselan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,21 +9,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import stego.StegoTool;
-import stego.imaging.Pixel;
+import stego.imaging.PixelKey;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.*;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.google.common.collect.Lists.newArrayList;
-import static com.google.common.collect.Maps.newHashMap;
+import java.util.Base64;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static stego.StegoTool.getLSB;
+import static stego.StegoTool.replaceLSB;
 
 @Controller
 public class MainController {
@@ -39,48 +36,34 @@ public class MainController {
         final ModelAndView imageOutput = new ModelAndView("imageOutput");
 
         BufferedImage messageImg = Sanselan.getBufferedImage(message.getInputStream());
-        final List<Pixel> messagePixels = newArrayList();
+        final Map<PixelKey, Color> messagePixels = new ConcurrentHashMap<>(messageImg.getWidth()*messageImg.getHeight());
         for (int x = 0; x < messageImg.getWidth(); x++) {
             for (int y = 0; y < messageImg.getHeight(); y++) {
-                messagePixels.add(new Pixel(x, y, messageImg.getRGB(x, y)));
+                Color messageColor = new Color(messageImg.getRGB(x, y));
+                messagePixels.put(new PixelKey(x, y), new Color(StegoTool.getMSB(messageColor.getRed(), 8), StegoTool.getMSB(messageColor.getGreen(), 8), StegoTool.getMSB(messageColor.getBlue(), 8)));
             }
         }
 
         BufferedImage coverImg = Sanselan.getBufferedImage(cover.getInputStream());
-        final Map<Pixel.PixelKey, Pixel> coverPixels = newHashMap();
+        final Map<PixelKey, Color> coverPixels = new ConcurrentHashMap<>(coverImg.getWidth()*coverImg.getHeight());
         for (int x = 0; x < coverImg.getWidth(); x++) {
             for (int y = 0; y < coverImg.getHeight(); y++) {
-                final Pixel pixel = new Pixel(x, y, coverImg.getRGB(x, y));
-                coverPixels.put(pixel.getKey(), pixel);
+                coverPixels.put(new PixelKey(x, y), new Color(coverImg.getRGB(x, y)));
             }
         }
 
-        final Map<Pixel.PixelKey, Pixel> pixelMap = messagePixels.parallelStream()
-                .map(p -> new Pixel(p.getX(), p.getY(), StegoTool.getMSB(p.getRed(), 8), StegoTool.getMSB(p.getGreen(), 8), StegoTool.getMSB(p.getBlue(), 8)))
-                .collect(Collectors.toMap(Pixel::getKey, p -> p));
-
-        for (int x = 0; x < coverImg.getWidth(); x++) {
-            for (int y = 0; y < coverImg.getHeight(); y++) {
-                final Pixel.PixelKey key = new Pixel.PixelKey(x, y);
-                final Pixel messageMSBPixel = pixelMap.get(key);
-                if (messageMSBPixel != null) {
-                    final Pixel coverPixel = coverPixels.get(key);
-
-                    final int redEmbed = coverPixel.getRed() | messageMSBPixel.getRed();
-                    final int greenEmbed = coverPixel.getGreen() | messageMSBPixel.getGreen();
-                    final int blueEmbed = coverPixel.getBlue() | messageMSBPixel.getBlue();
-
-                    coverImg.setRGB(x, y, new Pixel(coverPixel.getX(), coverPixel.getY(), redEmbed, greenEmbed, blueEmbed).getRGB());
-                }
-            }
-        }
+        coverPixels.entrySet().parallelStream().filter(entry -> messagePixels.get(entry.getKey()) != null).forEach(e -> {
+            PixelKey key = e.getKey();
+            final Color msgColor = messagePixels.get(key);
+            final Color coverColor = e.getValue();
+            final Color embedColor = new Color(replaceLSB(coverColor.getRed(), msgColor.getRed()), replaceLSB(coverColor.getGreen(), msgColor.getGreen()), replaceLSB(coverColor.getBlue(), msgColor.getBlue()));
+            coverImg.setRGB(key.getX(), key.getY(), embedColor.getRGB());
+        });
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         ImageIO.write(coverImg, "png", outputStream);
-
         imageOutput.addObject("image64", Base64.getEncoder().encodeToString(outputStream.toByteArray()));
         return imageOutput;
-
     }
 
     @RequestMapping(value = "/decode", method = RequestMethod.POST)
@@ -94,7 +77,6 @@ public class MainController {
                 final Color color = new Color(image.getRGB(x, y));
                 final Color lsbColor = new Color(255*getLSB(color.getRed()), 255*getLSB(color.getGreen()), 255*getLSB(color.getBlue()));
                 image.setRGB(x, y, lsbColor.getRGB());
-
             }
         }
 
